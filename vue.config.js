c4755c2f61f71219cfeda89bc95809e28d576dbe
lemurkyte <43951941+lemurkyte@users.noreply.github.com>
2019-05-09 15:32:21 +0200

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+/*
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? ''
+  : '/'
+  ,
+  css: {
+    loaderOptions: {
+      sass: {
+        data: 
+        `@import "@/styles/_variables.scss";`
+        `@import "@/styles/typo.scss";`
+      }
+    }
+  }
+};
+
+*/
